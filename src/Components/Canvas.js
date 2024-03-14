d5564508801e9e97ca5c5862ca0d67f0bfea9847
Ayush Yadav <aayush0029@gmail.com>
2024-03-14 16:40:10 +0530

    Added React Konva

--- src/Components/Canvas.js
+++ src/Components/Canvas.js
@@ -1,11 +1,50 @@
 import React from 'react'
 
 function Canvas() {
+  const [lines, setLines] = useState([]);
+  const [isDrawing, setIsDrawing] = useState(false);
+
+  const handleMouseDown = (event) => {
+    setIsDrawing(true);
+    const { x, y } = event.target.getStage().getPointerPosition();
+    setLines([...lines, { points: [x, y] }]);
+  };
+
+  const handleMouseMove = (event) => {
+    if (!isDrawing) return;
+    const { x, y } = event.target.getStage().getPointerPosition();
+    let lastLine = lines[lines.length - 1];
+    lastLine.points = lastLine.points.concat([x, y]);
+    setLines([...lines.slice(0, lines.length - 1), lastLine]);
+  };
+
+  const handleMouseUp = () => {
+    setIsDrawing(false);
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
+    <Stage
+      width={window.innerWidth}
+      height={window.innerHeight}
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+    >
+      <Layer>
+        {lines.map((line, i) => (
+          <Line
+            key={i}
+            points={line.points}
+            stroke="black"
+            strokeWidth={5}
+            tension={0.5}
+            lineCap="round"
+            globalCompositeOperation="source-over"
+          />
+        ))}
+      </Layer>
+    </Stage>
+  );
 }
 
-export default Canvas
+export default Canvas;
