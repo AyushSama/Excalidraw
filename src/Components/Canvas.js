a60ea83846c6f525e6182e80da5cd06bce16aa26
Ayush Yadav <aayush0029@gmail.com>
2024-03-17 23:12:45 +0530

    Now Rectangle and Circle is Working

--- src/Components/Canvas.js
+++ src/Components/Canvas.js
@@ -3,11 +3,10 @@ import { Stage, Layer, Line, Circle, Arrow, Rect, Transformer, RegularPolygon } 
 import { useActionContext } from '../Context/ActionContext'
 import { v4 as uuidv4 } from 'uuid';
 import Menu from './Menu';
-import userEvent from '@testing-library/user-event';
 
 export default function Canvas() {
 
-    const currentAction = useActionContext();  // Get the current Action
+    const { currentAction } = useActionContext();  // Get the current Action
     const stageRef = useRef();   // Reference for the Stage
     const [points, setPenPoints] = useState([]);
 
@@ -33,14 +32,16 @@ export default function Canvas() {
         currentShapeId.current = id;
         isDrawing.current = true;
 
+        console.log(currentShapeId.current)
+
         switch (currentAction) {
             case 'rect':
                 setRectangles((rectangles) => [...rectangles, {
                     id,
                     x,
                     y,
-                    height: 50,
-                    width: 50
+                    height: 0,
+                    width: 0
                 }]);
                 break;
             case 'circle':
@@ -48,7 +49,7 @@ export default function Canvas() {
                     id,
                     x,
                     y,
-                    radius: 10,
+                    radius: 0,
                 }])
         }
 
@@ -65,10 +66,10 @@ export default function Canvas() {
         console.log(currentAction + "  " + currentShapeId.current)
 
         switch (currentAction) {
-            case 'rect':
+            case 'rect' :
                 setRectangles((rectangles) =>
-                rectangles.map((rectangle) => {
-                    if (rectangle.id === currentShapeId.current) {
+                    rectangles.map((rectangle) => {
+                        if (rectangle.id === currentShapeId.current) {
                             return {
                                 ...rectangle,
                                 width: x - rectangle.x,
@@ -79,18 +80,20 @@ export default function Canvas() {
                     })
                 );
                 break;
-            case 'circle':
+            case "circle":
                 setCircles((circles) =>
                     circles.map((circle) => {
                         if (circle.id === currentShapeId.current) {
                             return {
                                 ...circle,
-                                radius: ((y - circle.y) ** 2 + (x - circle.x) ** 2) ** 0.5,
+                                radius: Math.sqrt((y - circle.y) ** 2 + (x - circle.x) ** 2),
                             };
                         }
                         return circle;
                     })
                 );
+            default:
+                return null;
         }
 
     }
